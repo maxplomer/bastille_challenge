a160ba77272115056d233bace9861158c93e3944
Max Plomer <maxplomer@gmail.com>
2016-07-27 19:56:33 -0400

    save

--- code.rb
+++ code.rb
@@ -1,17 +1,31 @@
-module UrlMethods
+# module UrlMethods
+#   require 'set'
+#   hash = Hash.new {|hash, key| hash[key] = Set.new }
+
+#   def self.saveUrl(user_token, url)
+#   	p hash
+#     return false if hash[user_token].include? url
+
+#     hash[user_token] << url
+
+#     true
+#   end
+
+# end
+
+# p UrlMethods.saveUrl('user-12345678', 'http://news.google.com')
+# p UrlMethods.saveUrl('user-12345678', 'http://news.google.com')
+
+
+class UrlMethods
   require 'set'
-  hash = Hash.new {|hash, key| hash[key] = Set.new }
 
-  def self.saveUrl(user_token, url)
-  	p hash
-    return false if hash[user_token].include? url
+  attr_accessor :db_hash
 
-    hash[user_token] << url
-
-    true
+  def initialize  
+    @db_hash = Hash.new {|hash, key| hash[key] = Set.new }
   end
 
 end
 
-p UrlMethods.saveUrl('user-12345678', 'http://news.google.com')
-p UrlMethods.saveUrl('user-12345678', 'http://news.google.com')
+app = UrlMethods.new
