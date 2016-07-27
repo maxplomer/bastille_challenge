64736d06aed670aa3e8925706433951729f02236
Max Plomer <maxplomer@gmail.com>
2016-07-27 19:45:51 -0400

    add

--- code.rb
+++ code.rb
@@ -3,14 +3,14 @@ module UrlMethods
   hash = Hash.new {|hash, key| hash[key] = Set.new }
 
   def saveUrl(user_token, url)
-    
+    return false if hash[user_token].include? url
 
+    hash[user_token] << url
+
+    true
   end
 
 end
 
-
-
-
-
-UrlMethods.saveUrl('user-12345678', 'http://news.google.com')
+p UrlMethods.saveUrl('user-12345678', 'http://news.google.com')
+p UrlMethods.saveUrl('user-12345678', 'http://news.google.com')
