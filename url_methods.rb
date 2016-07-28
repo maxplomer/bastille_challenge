94c680f547fd1bc6d44e4e3809bcfd3db986f19d
Max Plomer <maxplomer@gmail.com>
2016-07-27 20:53:52 -0400

    clean up start spec

--- url_methods.rb
+++ url_methods.rb
@@ -21,24 +21,4 @@ class UrlMethods
     @db_hash[user_token].delete url
     true
   end
-
 end
-
-require 'rspec'
-describe UrlMethods do
-  it 'returns false if already added' do
-    #deck.take(2)
-    expect(2).to eq(2)
-  end
-
-end
-
-app = UrlMethods.new
-p app.save_url('user-12345678', 'http://news.google.com')
-p app.save_url('user-12345678', 'http://news.google.com')
-p app.save_url('user-12345678', 'http://news.yahoo.com')
-p app.get_urls('user-12345678')
-p app.get_urls('user-1234567')
-p app.remove_url('user-12345678', 'http://news.yahoo.com')
-p app.remove_url('user-12345678', 'http://news.yahoo.com')
-
