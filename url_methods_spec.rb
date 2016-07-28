5292e32b5977cf39319b30f58be0ee894c675b55
Max Plomer <maxplomer@gmail.com>
2016-07-27 21:01:52 -0400

    finish and cleanup spec

--- url_methods_spec.rb
+++ url_methods_spec.rb
@@ -32,21 +32,17 @@ describe "#get_urls" do
   end
 end
 
-# require 'rspec'
-# describe UrlMethods do
-#   it 'returns false if already added' do
-#     #deck.take(2)
-#     expect(2).to eq(2)
-#   end
+describe "#remove_url" do
+  it "returns true if removed url" do
+    app = UrlMethods.new
+    app.save_url('user-12345678', 'http://news.google.com')
+    result = app.remove_url('user-12345678', 'http://news.google.com')
+    expect(result).to eq(true)
+  end
 
-# end
-
-# app = UrlMethods.new
-# p app.save_url('user-12345678', 'http://news.google.com')
-# p app.save_url('user-12345678', 'http://news.google.com')
-# p app.save_url('user-12345678', 'http://news.yahoo.com')
-# p app.get_urls('user-12345678')
-# p app.get_urls('user-1234567')
-# p app.remove_url('user-12345678', 'http://news.yahoo.com')
-# p app.remove_url('user-12345678', 'http://news.yahoo.com')
-
+  it "returns false is url doesn't exist" do
+    app = UrlMethods.new
+    result = app.remove_url('user-12345678', 'http://news.google.com')
+    expect(result).to eq(false)
+  end
+end
