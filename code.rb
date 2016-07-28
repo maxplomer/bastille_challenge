bf20ca3c33df03e5c3a590cb238d6d8ac01a370f
Max Plomer <maxplomer@gmail.com>
2016-07-27 20:12:20 -0400

    save

--- code.rb
+++ code.rb
@@ -15,6 +15,13 @@ class UrlMethods
   	@db_hash[user_token] if @db_hash.key? user_token 
   end
 
+  def remove_url(user_token, url)
+  	return false unless @db_hash.key? user_token
+  	return false unless @db_hash[user_token].include? url
+    @db_hash[user_token].delete url
+    true
+  end
+
 end
 
 app = UrlMethods.new
@@ -23,5 +30,6 @@ p app.save_url('user-12345678', 'http://news.google.com')
 p app.save_url('user-12345678', 'http://news.yahoo.com')
 p app.get_urls('user-12345678')
 p app.get_urls('user-1234567')
-
+p app.remove_url('user-12345678', 'http://news.yahoo.com')
+p app.remove_url('user-12345678', 'http://news.yahoo.com')
 
