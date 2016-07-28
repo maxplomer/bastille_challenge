280db7fd944e7ea38441acf3b7957860e8c7df5d
Max Plomer <maxplomer@gmail.com>
2016-07-27 20:02:35 -0400

    save

--- code.rb
+++ code.rb
@@ -11,12 +11,21 @@ class UrlMethods
     true
   end
 
+  def get_urls(user_token)
+  	if @db_hash.key?(user_token)
+      @db_hash[user_token]
+    else
+      nil
+    end
+  end
+
 end
 
 app = UrlMethods.new
 p app.save_url('user-12345678', 'http://news.google.com')
 p app.save_url('user-12345678', 'http://news.google.com')
-
-
+p app.save_url('user-12345678', 'http://news.yahoo.com')
+p app.get_urls('user-12345678')
+p app.get_urls('user-1234567')
 
 
