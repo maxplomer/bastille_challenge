b897a999b2e1a84915d57dc3fd56a011f1c18b76
Max Plomer <maxplomer@gmail.com>
2016-07-27 20:59:44 -0400

    save

--- url_methods_spec.rb
+++ url_methods_spec.rb
@@ -2,7 +2,6 @@ require 'rspec'
 require './url_methods'
 
 describe "#save_url" do
-
   it "returns true if added" do
     app = UrlMethods.new
     result = app.save_url('user-12345678', 'http://news.google.com')
@@ -15,7 +14,22 @@ describe "#save_url" do
     result = app.save_url('user-12345678', 'http://news.google.com')
     expect(result).to eq(false)
   end
+end
 
+describe "#get_urls" do
+  it "retrieves saved urls" do
+    app = UrlMethods.new
+    app.save_url('user-12345678', 'http://news.google.com')
+    app.save_url('user-12345678', 'http://news.yahoo.com')
+    result = app.get_urls('user-12345678')
+    expect(result).to eq(Set.new ["http://news.google.com", "http://news.yahoo.com"])
+  end
+
+  it "returns nil if no urls saved" do
+    app = UrlMethods.new
+    result = app.get_urls('user-12345678')
+    expect(result).to eq(nil)
+  end
 end
 
 # require 'rspec'
